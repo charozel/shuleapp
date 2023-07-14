c9415dbf6e1fd0bf04f1d407f18ac52648ab64bc
charozel <charozel@gmail.com>
2023-07-14 18:33:32 +0300

    google auth-initial

--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -1,9 +1,46 @@
-import { View, Text, Image, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
 import Colors from "../Shared/Colors";
 import { AntDesign } from "@expo/vector-icons";
-
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { AuthContext } from "../Context/AuthContext";
 export default function Login() {
+	WebBrowser.maybeCompleteAuthSession();
+	const [accessToken, setAccessToken] = useState();
+	const [userInfo, setUserInfo] = useState();
+	const [userData, setUserData] = useContext(AuthContext);
+	const [request, response, promptAsync] = Google.useAuthRequest({
+		androidClientId:
+			"14418695642-bofr7vrmt0thajgthoftlej69bcgrrkl.apps.googleusercontent.com",
+		expoClientId:
+			"14418695642-rfarcjko2mr68qkld40t6j7kr8d37c9v.apps.googleusercontent.com",
+	});
+	useEffect(() => {
+		if (response?.type == "success") {
+			setAccessToken(response.authentication.accessToken);
+
+			getUserData();
+		}
+	}, [response]);
+	const getUserData = async () => {
+		try {
+			const resp = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+				headers: {
+					Authorization: `Bearer ${response.authentication.accessToken}`,
+				},
+			});
+			const user = await resp.json();
+			console.log("user Details, user");
+			setUserInfo(user);
+			setUserData(user);
+		} catch {
+			error;
+		}
+		{
+			//add error handle here
+		}
+	};
 	return (
 		<View>
 			<Image
@@ -17,7 +54,7 @@ export default function Login() {
 			/>
 			<View style={styles.container}>
 				<Text style={styles.loginText}>Login/Sign up</Text>
-				<View style={styles.button}>
+				<TouchableOpacity style={styles.button} onPress={() => promptAsync()}>
 					<AntDesign
 						name='google'
 						size={24}
@@ -25,7 +62,7 @@ export default function Login() {
 						style={{ marginRight: 10 }}
 					/>
 					<Text style={styles.signInText}>Sign in with Google</Text>
-				</View>
+				</TouchableOpacity>
 			</View>
 		</View>
 	);
