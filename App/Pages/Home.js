b4a6a95b691d1720755c2cafaa0303a482726a5e
charozel <charozel@gmail.com>
2023-07-17 11:00:23 +0300

    auth, login-integration

--- App/Pages/Home.js
+++ App/Pages/Home.js
@@ -1,12 +1,20 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import { Button } from "react-native";
 import Services from "../Shared/Services";
+import { AuthContext } from "../Context/AuthContext";
 
 export default function Home() {
+	const { userData, setUserData } = useContext(AuthContext);
 	return (
 		<View>
-			<Button title='Logout' onPress={() => Services.Logout()} />
+			<Button
+				title='Logout'
+				onPress={() => {
+					Services.Logout();
+					setUserData(null);
+				}}
+			/>
 		</View>
 	);
 }
